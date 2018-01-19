c55f7ae568af75a9fcdca34a1d518067879aa147
Takumi Ozato <1992tkm0819@gmail.com>
2018-01-19 07:55:59 +0000

    投稿の登録機能を実装

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,34 @@
 class HomeController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
   def top
     @posts = Post.all
     @count = Post.maximum('id')
     
     @post = Post.new
   end
+
+  def create
+    @post = Post.new(post_params)
+
+    respond_to do |format|
+      if @post.save
+        format.html { redirect_to '/', notice: 'Post was successfully created.' }
+        format.json { render '/', status: :created, location: '/' }
+      else
+        format.html { render :new }
+        format.json { render json: @post.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_post
+      @post = Post.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def post_params
+      params.require(:post).permit(:contents)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get '/' => 'home#top'
+  post '/' => 'home#create'
   resources :posts
 end
